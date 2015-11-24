276345ab61e61657d4fa7bf10ae17007e7019ae3
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-11-23 17:46:02 -0800

    react version updated to be 0.14.3

--- test/component-test.js
+++ test/component-test.js
@@ -3,23 +3,24 @@ require('./dom-mock')('<html><body></body></html>');
 
 var jsdom = require('mocha-jsdom');
 var assert = require('assert');
-var React = require('react/addons');
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
 
 describe('Testing my div', function() {
   jsdom({ skipWindowCheck: true });
 
   it('should contain text: Lovely! Here it is - my very first React component!', function() {
     var VeryFirstDiv = require('../components/component.jsx');
-    var TestUtils = React.addons.TestUtils;
+    //var TestUtils = React.addons.TestUtils;
 
 
     var myDiv = TestUtils.renderIntoDocument(
       <VeryFirstDiv />
     );
-    
+
     var divText = TestUtils.findRenderedDOMComponentWithTag(
       myDiv, 'span');
-  
+
     assert.equal(divText.getDOMNode().textContent, 'Lovely! Here it is - my very first React component!');
   });
 });
